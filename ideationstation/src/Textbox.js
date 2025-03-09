be4b44c2df5542dbd40b5932b2975783ab1d00ae
janapc03 <janapc03@gmail.com>
2025-03-08 18:52:45 -0800

    added textbox js and css files

---
+++ ideationstation/src/Textbox.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import './Textbox.css'; // !!! change to textbox css file after creating
+
+function Textbox() {
+  const [val, setVal] = useState("Enter your thoughts!")
+  const submit = () => {
+    alert(val)
+  }
+  const change = event => {
+    setVal(event.target.value)
+  }
+  return (
+    <div className="Textbox">
+      <input onChange = {change} 
+      value = {val} />
+      <button onClick = {submit}>Submit</button> 
+    </div>
+  );
+}
+
+export default Textbox;
