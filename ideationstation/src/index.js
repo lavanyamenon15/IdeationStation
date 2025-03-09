35d50591c75735fce53cda94df17bfb7f2c016cc
janapc03 <janapc03@gmail.com>
2025-03-08 21:22:33 -0800

    updated textbox css

--- ideationstation/src/index.js
+++ ideationstation/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Stars from './Stars';
+import Textbox from './Textbox';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Stars />
+    <Textbox />
   </React.StrictMode>
 );
 
